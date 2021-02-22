e53e0e5eddec82387303fd84ed30e072b789505e
ritheshnv <rithesh.mys@gmail.com>
2021-02-22 21:49:44 +0000

    GG assessment

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import ShowBooks from "./containers/ShowBooks";
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<header className="App-header">
+					<Switch>
+						<Route path="/" exact key="1" component={ShowBooks} />
+						<Route path="/:pageNumber" key="2" component={ShowBooks} />
+					</Switch>
+				</header>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
